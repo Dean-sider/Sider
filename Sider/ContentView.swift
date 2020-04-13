3f0658f78b176aed0ddee8bd67fd2ce6dfe8819b
andy <andy870622@gmail.com>
2020-04-13 13:55:54 +0800

    Initial Commit

---
+++ Sider/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sider
+//
+//  Created by User15 on 2020/4/13.
+//  Copyright © 2020 test. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
